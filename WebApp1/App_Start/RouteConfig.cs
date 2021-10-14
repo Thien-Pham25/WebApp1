57cc874f568451ba5f74fd4047e6047f1ffe6fbe
Thien <thienpdgcd191117@fpt.edu.vn>
2021-10-14 22:06:13 +0700

    Update layout and RouteConfig

--- WebApp1/App_Start/RouteConfig.cs
+++ WebApp1/App_Start/RouteConfig.cs
@@ -16,8 +16,10 @@ namespace WebApp1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebApp1.Controllers"}
+               
+            ) ;
         }
     }
 }
