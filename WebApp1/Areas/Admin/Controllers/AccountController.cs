e8b9b59dd7d38618ba49a97ece3713962a889fde
Thien <thienpdgcd191117@fpt.edu.vn>
2021-10-15 00:08:11 +0700

    Add Create Staff and Trainer Account Functionality

--- WebApp1/Areas/Admin/Controllers/AccountController.cs
+++ WebApp1/Areas/Admin/Controllers/AccountController.cs
@@ -1,17 +1,86 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebApp1.Models;
+using WebApp1.Utils;
+using WebApp1.ViewModels;
 
 namespace WebApp1.Areas.Admin.Controllers
 {
     public class AccountController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         // GET: Admin/Account
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new AccountViewModel()
+            {
+                Roles = new List<string>() { Role.Trainer, Role.Staff }
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create (AccountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(user.Id, model.Role);
+
+                    
+                    return RedirectToAction("Index");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
         }
     }
 }
---
+++ WebApp1/ViewModels/GroupedUsersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApp1.Models;
+
+namespace WebApp1.ViewModels
+{
+    public class GroupedUsersViewModel
+    {
+        public List<ApplicationUser> Staffs { get; set; }
+        public List<ApplicationUser> Trainers { get; set; }
+       
+    }
+}
